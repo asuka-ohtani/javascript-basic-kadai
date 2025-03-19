10179a55350014cc4726fb2b4887ef42e83844f9
asuka-ohtani <asu_channn@yahoo.co.jp>
2025-03-19 22:31:04 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,5 +0,0 @@
-let text = document.getElementById('text').textContent;
-const btn = document.getElementById('btn');
-btn.addEventListener('click', () =>{
-    document.getElementById('text').textContent = 'ボタンをクリックしました';
-})
