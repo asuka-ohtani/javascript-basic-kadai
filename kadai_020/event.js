62a72c15e2af5eb2f59ca1aeaa98ae4193162567
asuka-ohtani <asu_channn@yahoo.co.jp>
2025-03-19 22:31:37 +0900

    kadai_020をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const text = document.getElementById('text');
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () =>{
+    document.getElementById('text').textContent = 'ボタンをクリックしました';
+})
